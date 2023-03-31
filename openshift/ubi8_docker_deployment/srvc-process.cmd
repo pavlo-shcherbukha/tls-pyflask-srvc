29126a85902918bdfd3c6fc5f84411f044e99fcb
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2023-03-31 11:45:49 +0300

    depl

--- openshift/ubi8_docker_deployment/srvc-process.cmd
+++ openshift/ubi8_docker_deployment/srvc-process.cmd
@@ -6,10 +6,10 @@ pause
 set fltempl=py-ubi8docker-srvc-templ.yaml
 set fldepl=py-ubi8docker-srvc-depl.yaml 
 oc delete -f %fldepl%
-set APP_SERVICE_NAME=py-ubi8docker-srvc
-set APP_NAME=py-remote-debug
+set APP_SERVICE_NAME=pytls-ubi8docker-srvc
+set APP_NAME=py-tls
 set GIT_BRANCH=tz-000001-init
-set GIT_URL=https://github.com/pavlo-shcherbukha/py-flask-rdbg.git
+set GIT_URL=https://github.com/pavlo-shcherbukha/tls-pyflask-srvc.git
 set DOCKER_PTH=./openshift/ubi8_docker_deployment/Dockerfile
 
 oc delete -f %fldepl%
